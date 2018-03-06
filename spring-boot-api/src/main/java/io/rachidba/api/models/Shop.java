afbde2b2d3b75a5d8b3f58a356a217d09a02db15
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-06 12:43:37 +0000

    Add Shop class to io.rachidba.apimodels package
      	Shop class describe a shop object
    		{
    			"id" : String,
    			"picture" : String,
    			"name" : String,
    			"email" : String,
    			"city": String,
    			"location" : Location
    		}

---
+++ spring-boot-api/src/main/java/io/rachidba/api/models/Shop.java
@@ -0,0 +1,74 @@
+package io.rachidba.api.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="shops")
+public class Shop {
+    @Id
+    private String id;
+    private String picture;
+    private String name;
+    private String email;
+    private String city;
+    private Location location;
+
+    public Shop() {
+        super();
+    }
+
+    public Shop(String picture, String name, String email, String city) {
+        this.picture = picture;
+        this.name = name;
+        this.email = email;
+        this.city = city;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
