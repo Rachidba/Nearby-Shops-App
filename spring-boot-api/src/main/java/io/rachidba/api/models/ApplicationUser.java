61003789e00e192793451fd87bb41c9e82b7392e
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-06 13:03:46 +0000

    Add ApplicationUser class
      	ApplicationUser class represente the Users:
    		username: the user email (unique value)
    		password: The user password for authetication
    		likedShops: Liste of current liked shops

---
+++ spring-boot-api/src/main/java/io/rachidba/api/models/ApplicationUser.java
@@ -0,0 +1,67 @@
+package io.rachidba.api.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection="users")
+public class ApplicationUser {
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String username;
+    private String password;
+    private List<String> likedShops;
+
+    public ApplicationUser() {
+        super();
+    }
+
+    public ApplicationUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.likedShops = new ArrayList<>();
+    }
+
+    public ApplicationUser(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.likedShops = new ArrayList<>();
+    }
+
+    public ApplicationUser(String id, String username, String password, List<String> likedShops) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.likedShops = likedShops;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String email) {
+        this.username = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
