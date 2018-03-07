ee1d6d2d1b3b042de1f6d6cf65ba9b3553001c4e
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-07 23:39:44 +0000

    Add an endpoint that enables new users to register
      	This endpoint will be handled by a new @Controller class
    	Add UserController class to io.rachidba.api package

---
+++ spring-boot-api/src/main/java/io/rachidba/api/controllers/UserController.java
@@ -0,0 +1,31 @@
+package io.rachidba.api.controllers;
+
+import io.rachidba.api.models.ApplicationUser;
+import io.rachidba.api.repositories.ApplicationUserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+// The endpoint that enables new users to register
+public class UserController {
+
+    private ApplicationUserRepository applicationUserRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(ApplicationUserRepository applicationUserRepository,
+                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.applicationUserRepository = applicationUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody ApplicationUser user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        applicationUserRepository.save(user);
+    }
+}
