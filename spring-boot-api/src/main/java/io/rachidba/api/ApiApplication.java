5590fc9135723d94752f6ae3074cafe97bf6c176
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-07 23:44:56 +0000

    Implementing a method that generates an instance of BCryptPasswordEncoder
       	Add bCryptPasswordEncoder() in the ApiApplication class

--- spring-boot-api/src/main/java/io/rachidba/api/ApiApplication.java
+++ spring-boot-api/src/main/java/io/rachidba/api/ApiApplication.java
@@ -2,10 +2,18 @@ package io.rachidba.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	// This method generates an instance of BCryptPasswordEncoder
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
