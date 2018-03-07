45f86e0be481345978e50ce7fc793e4d43dd8fc3
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-07 01:56:36 +0000

    Add Get Mapping form /api/shops
      	this route return the list of all shops

--- spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
+++ spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
@@ -1,9 +1,13 @@
 package io.rachidba.api.controllers;
 
+import io.rachidba.api.models.Shop;
 import io.rachidba.api.repositories.ShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,5 +15,10 @@ public class ShopController {
 
     @Autowired
     ShopRepository shopRepository;
+
+    @GetMapping("/shops")
+    public List<Shop> getAllShops() {
+        return shopRepository.findAll();
+    }
 
 }
