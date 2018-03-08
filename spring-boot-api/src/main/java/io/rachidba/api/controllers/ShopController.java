abe6514782df5c3cb9feed193c40a6f1ff50fb3e
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-08 02:16:31 +0000

    Add Get Mapping to /api/shops/liked route
      	This request is used by user to get the list of liked shops

--- spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
+++ spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
@@ -1,8 +1,12 @@
 package io.rachidba.api.controllers;
 
+import io.rachidba.api.models.ApplicationUser;
 import io.rachidba.api.models.Shop;
+import io.rachidba.api.repositories.ApplicationUserRepository;
 import io.rachidba.api.repositories.ShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +18,20 @@ import java.util.List;
 public class ShopController {
 
     @Autowired
-    ShopRepository shopRepository;
-
+    private ShopRepository shopRepository;
+    @Autowired
+    private ApplicationUserRepository userRepository;
     @GetMapping("/shops")
     public List<Shop> getAllShops() {
         return shopRepository.findAll();
+    }
+
+    @GetMapping("/shops/liked")
+    public List<Shop> getLikedShops() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        List<String> likedShops = userRepository.findByUsername(username).getLikedShops();
+        return (List) shopRepository.findAllById(likedShops);
     }
 
 }
