3fbbe5d52afbda77684ca9524ecf1e352fd60911
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-15 22:37:53 +0000

    Implement ngOnInit, ShopsComponent s method
      	To get list of all shops from Spring boot API we have to pass an
    	Authorization key
    	We get The Authorization key by posting a user's user name and
    	password to API (/api/login)

--- angular-client/src/app/shops/shops.component.ts
+++ angular-client/src/app/shops/shops.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../services/shop.service';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-shops',
@@ -7,11 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopsComponent implements OnInit {
 
-  constructor() { }
+  shops: any;
+  constructor(private http: HttpClient) { 
+
+  }
 
   ngOnInit() {
-
-    
+    const url = 'http://localhost:8091/api/shops';
+    this.http.get(url, 
+        {headers: new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjIiLCJleHAiOjE1MjE3Mzc4NTV9.4g31kmJuVHE-tIaSgUM2KNzxQo0I2l5Tj7lB4Vb_mChswAzgUsIHsvFvtR46ifogODO9nPLIx2drnrFm_ka8cw')})
+        .subscribe(res => {
+          this.shops = Object.keys(res).map(function(k) { return res[k] });
+    },
+      (err: HttpErrorResponse) => {
+        console.log(err.error);
+        console.log(err.name);
+        console.log(err.message);
+        console.log(err.status);
+      }
+    );
   }
 
 }
