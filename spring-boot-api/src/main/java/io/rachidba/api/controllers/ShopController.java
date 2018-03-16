7b1d447b76188d360fa73fc9ad660601d2a81710
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-16 02:52:19 +0000

    Add pagination to shops controller

--- spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
+++ spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
@@ -1,16 +1,17 @@
 package io.rachidba.api.controllers;
 
-import io.rachidba.api.models.ApplicationUser;
 import io.rachidba.api.models.Shop;
 import io.rachidba.api.repositories.ApplicationUserRepository;
 import io.rachidba.api.repositories.ShopRepository;
+import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @RestController
@@ -22,12 +23,14 @@ public class ShopController {
     @Autowired
     private ApplicationUserRepository userRepository;
     @GetMapping("/shops")
-    public List<Shop> getAllShops() {
-        return shopRepository.findAll();
+    public List<Shop> getAllShops(Pageable pageable) {
+        Page<Shop> shops = shopRepository.findAll(pageable);
+        return Lists.newArrayList(shops.iterator());
     }
 
+
     @GetMapping("/shops/liked")
-    public List<Shop> getLikedShops() {
+    public List<Shop> getLikedShops(Pageable pageable) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = auth.getName();
         List<String> likedShops = userRepository.findByUsername(username).getLikedShops();
