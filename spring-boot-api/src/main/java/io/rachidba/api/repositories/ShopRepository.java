d51f819c728b2c29f15a18c396831fba71f98a5b
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-17 20:54:16 +0000

    Add findByLocationNear method to ShopRepository
      	We pass a pageable, a certain Point (longitude and latitude) and
    	how far in distance it should look from that certain point in
    	miles or kilometers

--- spring-boot-api/src/main/java/io/rachidba/api/repositories/ShopRepository.java
+++ spring-boot-api/src/main/java/io/rachidba/api/repositories/ShopRepository.java
@@ -1,11 +1,16 @@
 package io.rachidba.api.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
 import io.rachidba.api.models.Shop;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
+
 @Repository
 public interface ShopRepository extends MongoRepository<Shop, String> {
-
+    Page<Shop> findByLocationNear(Point location, Distance distance, Pageable pageable);
 }
 
