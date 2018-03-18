af5511e1d536b4770121ece0312ff0a3b3741c9d
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-18 01:28:35 +0000

    Modify getAllShops to verify criteria: liked shops shouldnt be displayed on the main page
      Lodify getLikedShops methode to get preferred shops sorted by distance

--- spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
+++ spring-boot-api/src/main/java/io/rachidba/api/controllers/ShopController.java
@@ -1,6 +1,8 @@
 package io.rachidba.api.controllers;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
@@ -9,11 +11,15 @@ import io.rachidba.api.repositories.ApplicationUserRepository;
 import io.rachidba.api.repositories.ShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.geo.Metrics;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Iterator;
 import java.util.List;
+
+import static org.springframework.data.domain.PageRequest.of;
 
 @RestController
 @RequestMapping("/api")
@@ -29,17 +35,24 @@ public class ShopController {
                                   @RequestParam("lat") String latitude,
                                   @RequestParam("long") String longitude,
                                   @RequestParam("d") double distance) {
-        return this.shopRepository.findByLocationNear(
-                new Point(Double.valueOf(longitude), Double.valueOf(latitude)),
-                new Distance(distance, Metrics.KILOMETERS), pageable);
-    }
-
-    @GetMapping("/shops/liked")
-    public List<Shop> getLikedShops(Pageable pageable) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = auth.getName();
         List<String> likedShops = userRepository.findByUsername(username).getLikedShops();
-        return (List) shopRepository.findAllById(likedShops);
+        return this.shopRepository.findByLocationNearAndIdNotIn(
+                new Point(Double.valueOf(longitude), Double.valueOf(latitude)),
+                new Distance(distance, Metrics.KILOMETERS), likedShops, pageable);
     }
 
+    @GetMapping("/shops/liked")
+    public Page<Shop> getLikedShops(Pageable pageable,
+                                  @RequestParam("lat") String latitude,
+                                  @RequestParam("long") String longitude,
+                                  @RequestParam("d") double distance) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        List<String> likedShops = userRepository.findByUsername(username).getLikedShops();
+        return this.shopRepository.findByLocationNearAndIdIn(
+                new Point(Double.valueOf(longitude), Double.valueOf(latitude)),
+                new Distance(distance, Metrics.KILOMETERS), likedShops, pageable);
+    }
 }
