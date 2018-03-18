743fe30a46be8ee0979c0b8c80c78efc4424a66a
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-18 01:31:53 +0000

    Update shops componnent to:
    	display the list of shops sorted by distance
    	liked shops shouldnt be displayed on the main page
    	use pagination to facilitate navigation

--- angular-client/src/app/shops/shops.component.ts
+++ angular-client/src/app/shops/shops.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { ApiService } from '../services/api.service';
+import { Shop } from '../models/shop';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-shops',
@@ -8,25 +11,43 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 })
 export class ShopsComponent implements OnInit {
 
-  shops: any;
-  constructor(private http: HttpClient) { 
+  shops: Shop[];
+  pageEvent: PageEvent;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  first: boolean;
+  number: 0;
+  size: 20;
+
+  constructor(private apiService: ApiService) { 
 
   }
 
   ngOnInit() {
-    const url = 'http://localhost:8091/api/shops?page=0&size=20';
-    this.http.get(url, 
-        {headers: new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjIiLCJleHAiOjE1MjE3Mzc4NTV9.4g31kmJuVHE-tIaSgUM2KNzxQo0I2l5Tj7lB4Vb_mChswAzgUsIHsvFvtR46ifogODO9nPLIx2drnrFm_ka8cw')})
-        .subscribe(res => {
-          this.shops = Object.keys(res).map(function(k) { return res[k] });
-    },
-      (err: HttpErrorResponse) => {
-        console.log(err.error);
-        console.log(err.name);
-        console.log(err.message);
-        console.log(err.status);
+    this.loadShops(0);
+  }
+
+  loadShops(index: number) {
+    this.apiService.getAllShops(index).subscribe(
+      result => {
+        this.shops = result.shops;
+        this.totalElements = result.totalElements;
+        this.totalPages = result.totalPages;
+        this.last = result.last;
+        this.size = result.size;
+        this.number = result.number;
+        console.log(result.shops);
+      },
+      error => {
+        console.log(error);
       }
     );
+  }
+
+  onPaginateChange(event){
+    //alert(JSON.stringify("Current page index: " + event.pageIndex));
+    this.loadShops(event.pageIndex)
   }
 
 }
