73b1e0e453c727ad0668b1b4554e449a6e05bf95
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-18 01:27:00 +0000

    Add findByLocationNearAndIdIn to shopRepository
      	This methode return list of preferred shops sorted by distance

--- spring-boot-api/src/main/java/io/rachidba/api/repositories/ShopRepository.java
+++ spring-boot-api/src/main/java/io/rachidba/api/repositories/ShopRepository.java
@@ -1,16 +1,18 @@
 package io.rachidba.api.repositories;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.Point;
 import io.rachidba.api.models.Shop;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 @Repository
 public interface ShopRepository extends MongoRepository<Shop, String> {
-    Page<Shop> findByLocationNear(Point location, Distance distance, Pageable pageable);
+    Page<Shop> findByLocationNearAndIdNotIn(Point location, Distance distance, List<String> ids, Pageable pageable);
+    Page<Shop> findByLocationNearAndIdIn(Point location, Distance distance, List<String> ids, Pageable pageable);
 }
 
