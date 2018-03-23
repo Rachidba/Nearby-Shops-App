7ca224b7d43024043e3942c8688b445217ce82f8
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-23 02:50:06 +0000

    Add Login guard to '/login' and '/register' routes
      	User can access to login and register pages only if he is logged out

--- angular-client/src/app/routes/routes-routing.module.ts
+++ angular-client/src/app/routes/routes-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 import { ShopsComponent } from '../shops/shops.component';
 import { PreferredShopsComponent } from '../preferred-shops/preferred-shops.component';
+import { AuthGuardService } from '../services/auth-guard.service';
+import { LoginGuardService } from '../services/login-guard.service';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'shops', component: ShopsComponent},
-  { path: 'preferred-shops', component: PreferredShopsComponent}
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: '**', redirectTo: '/home', pathMatch: 'full'}
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuardService]},
+  { path: 'register', component: RegisterComponent, canActivate: [LoginGuardService]},
+  { path: 'shops', component: ShopsComponent, canActivate: [AuthGuardService] },
+  { path: 'preferred-shops', component: PreferredShopsComponent, canActivate: [AuthGuardService] },
+  { path: '', redirectTo: '/shops', pathMatch: 'full'},
+  { path: '**', redirectTo: '/shops', pathMatch: 'full'}
 ];
 
 @NgModule({
