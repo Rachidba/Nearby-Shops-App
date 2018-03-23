4e1e64b7b727dbcee787af09827c4c3f12e8b13a
rachidba <rachidbaaziz.contact@gmail.com>
2018-03-23 02:23:03 +0000

    Implement registration component

--- angular-client/src/app/register/register.component.ts
+++ angular-client/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { Login } from '../models/login';
+import { RegistrationService } from '../services/registration.service';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +9,9 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  success: boolean = false;
+  error: boolean = false;
+  constructor(private registerationService: RegistrationService) { }
 
   ngOnInit() {
   }
@@ -20,4 +23,20 @@ export class RegisterComponent implements OnInit {
     Validators.required,
     Validators.minLength(6),
   ]);
+
+  onRegister() {
+    if (this.emailFormControl.valid && this.passwordFormControl.valid) {
+      var login = new Login(this.emailFormControl.value, this.passwordFormControl.value);
+      var response = this.registerationService.register(login).subscribe(
+        result => {
+          this.success = true;
+          this.error = false;
+        },
+        error => {
+          this.success = false;
+          this.error = true;
+        }
+      );
+    }
+  }
 }
